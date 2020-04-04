8867029aacd2a32464443a3be970f7fa1bc6ef0d
Terry <terry@Terrys-MacBook-Pro.local>
2020-04-05 00:36:30 +0800

    feat: 按需引入vant；viewport适配

---
+++ vue.config.js
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const merge = require('webpack-merge')
+const tsImportPluginFactory = require('ts-import-plugin')
+const autoprefixer = require('autoprefixer')
+const pxtoviewport = require('postcss-px-to-viewport')
+
+module.exports = {
+  outputDir: 'dist',
+  publicPath: '/mobile/',
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          autoprefixer(),
+          pxtoviewport({
+            viewportWidth: 375,
+          }),
+        ],
+      },
+    },
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('ts')
+      .use('ts-loader')
+      .tap(options => {
+        options = merge(options, {
+          transpileOnly: true,
+          getCustomTransformers: () => ({
+            before: [
+              tsImportPluginFactory({
+                libraryName: 'vant',
+                libraryDirectory: 'es',
+                style: true,
+              }),
+            ],
+          }),
+          compilerOptions: {
+            module: 'es2015',
+          },
+        })
+        return options
+      })
+  },
+}
